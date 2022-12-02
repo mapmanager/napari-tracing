478ce3a6d4fd66a287b7481ba8c1d446b5d59e0f
Vasudha Jha <reachvasudha27@gmail.com>
2022-12-01 20:54:15 -0800

    Get the starting and ending points to run the algorithm on

--- src/napari_tracing/_widget.py
+++ src/napari_tracing/_widget.py
@@ -1,33 +1,181 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
+import warnings
+from typing import Optional
 
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
-from typing import TYPE_CHECKING
-
+import numpy as np
+from napari.layers import Layer
+from napari.utils.events import Event
+from napari.viewer import Viewer
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
-if TYPE_CHECKING:
-    pass
+from ._logger import logger
+from .utils import Utils
+
+RED = np.array([1, 0, 0, 1])
+GREEN = np.array([0, 1, 0, 1])
+WHITE = np.array([1, 1, 1, 1])
+BLACK = np.array([0, 0, 0, 1])
+PURPLE = np.array([0.5, 0, 0.5, 1])
+ORANGE = np.array([1, 0.65, 0, 1])
+TURQUOISE = np.array([0.25, 0.88, 0.82, 1])
 
 
 class TracerWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+    def __init__(self, viewer: Viewer, layer: Optional[Layer] = None) -> None:
+        """
+        Note that each layer has its command manager, which is why
+        we are creating a dictionary of command managers
+        which maps layerID to command manager instance.
+        """
         super().__init__()
-        self.viewer = napari_viewer
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        warnings.filterwarnings(action="ignore", category=FutureWarning)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        self.viewer = viewer
+        self.layer = None
+        self.layer_data = np.array([])
+        self.start = None
+        self.end = None
+        self.configure_gui()
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        if layer:
+            logger.info("layer found")
+            logger.info(layer)
+            self.layer = layer
+        else:
+            logger.info("layer not found. Finding active layer")
+            active_layer = self.find_active_layers()
+            if active_layer:
+                logger.info("active layer found")
+                logger.info(active_layer)
+                self.layer = active_layer
+
+        self.viewer.layers.events.inserted.connect(self.slot_insert_layer)
+        self.viewer.layers.events.removed.connect(self.slot_remove_layer)
+        self.viewer.layers.selection.events.changed.connect(
+            self.slot_select_layer
+        )
+
+    def configure_gui(self) -> None:
+        """
+        Configure a QHBoxLayout to hold the undo and redo buttons.
+        """
+        layout = QHBoxLayout()
+        trace_button = QPushButton("Trace")
+        trace_button.clicked.connect(self.trace)
+        layout.addWidget(trace_button)
+        self.setLayout(layout)
+
+    def trace(self):
+        logger.info("Beginning tracing...")
+
+    def find_active_layers(self) -> Optional[Layer]:
+        """
+        Find pre-existing selected layer.
+        """
+        currently_selected_layer = self.viewer.layers.selection.active
+        if currently_selected_layer:
+            return currently_selected_layer
+        return None
+
+    def connect_layer(self, layer: Layer) -> None:
+        """
+        Connect to a layer's events.
+        Here, we're connecting to the data change event.
+
+                Args:
+                        layer: Layer (Layer to connect to.)
+        """
+        if self.layer:
+            self.layer.events.data.disconnect(self.slot_points_data_change)
+            # self.layer.events.highlight.disconnect(self.slot_user_selected_point)
+            self.layer_data = np.array([])
+
+        self.layer = layer
+        if "data" in vars(layer).keys() and layer.data.any():
+            self.layer_data = layer.data.copy()
+        self.layer.events.data.connect(self.slot_points_data_change)
+        # self.layer.events.highlight.connect(self.slot_user_selected_point)
+
+    def slot_points_data_change(self, event: Event) -> None:
+        """
+        Modify the start/end points for a* search
+        based on point adding/deletion
+        """
+        if Utils.are_sets_equal(event.source.selected_data, self.layer_data):
+            # no change
+            return
+
+        if len(event.source.data) > len(self.layer_data):
+            logger.info("new point added")
+            idx, point = Utils.get_diff(event.source.data, self.layer_data)
+
+            if self.start is None and not np.array_equal(point, self.end):
+                logger.info(f"Making {point} as our start")
+                self.start = point
+                self.change_color(idx, ORANGE)
+
+            elif self.end is None and not np.array_equal(point, self.start):
+                logger.info(f"Making {point} as our end")
+                self.end = point
+                self.change_color(idx, TURQUOISE)
+
+            self.layer_data = event.source.data.copy()
+
+        elif len(event.source.data) < len(self.layer_data):
+            logger.info("point is deleted")
+            idx, point = Utils.get_diff(
+                self.layer_data, event.source.data.copy()
+            )
+            if np.array_equal(point, self.start):
+                logger.info("resetting start since it was deleted")
+                self.start = None
+
+            elif np.array_equal(point, self.end):
+                logger.info("resetting end since it was deleted")
+                self.end = None
+
+            self.layer_data = event.source.data.copy()
+
+    def slot_insert_layer(self, event: Event) -> None:
+        """
+        Respond to new layer in viewer.
+
+        Args:
+            event (Event): event.type == 'inserted'
+        """
+        logger.info(
+            f'New layer "{event.source}" was inserted at index {event.index}'
+        )
+        newly_inserted_layer = event.value
+        if newly_inserted_layer:
+            self.connect_layer(newly_inserted_layer)
+
+    def slot_remove_layer(self, event: Event) -> None:
+        """
+        Respond to layer delete in viewer.
+
+        Args:
+            event (Event): event.type == 'removed'
+        """
+        logger.info(f'Removed layer "{event.source}"')
+        currently_selected_layer = self.find_active_layers()
+        if currently_selected_layer and currently_selected_layer != self.layer:
+            self.connect_layer(currently_selected_layer)
+
+    def slot_select_layer(self, event: Event) -> None:
+        """Respond to layer selection in viewer.
+
+        Args:
+            event (Event): event.type == 'changed'
+        """
+        currently_selected_layer = self.find_active_layers()
+        if currently_selected_layer and currently_selected_layer != self.layer:
+            logger.info(f"New layer selected: {currently_selected_layer}")
+            self.connect_layer(currently_selected_layer)
+
+    def change_color(self, idx: int, color: np.array):
+        layer_colors = self.layer._face.colors
+        logger.info(f"layer colors: {layer_colors}")
+        layer_colors[idx] = color
+        self.layer._face.current_color = WHITE
+        self.layer.refresh()
---
+++ src/napari_tracing/utils.py
@@ -0,0 +1,53 @@
+from typing import List, Set, Tuple
+
+import numpy as np
+
+from ._logger import logger
+
+
+class Utils:
+    def are_sets_equal(a: Set, b: Set):
+        """
+        Compare if two sets are equal or not
+        """
+        if a is None or b is None:
+            return False
+
+        if len(a) != len(b):
+            return False
+
+        for x in a:
+            if x not in b:
+                return False
+
+        return True
+
+    def get_diff(
+        layer1_data: np.ndarray, layer2_data: np.ndarray
+    ) -> Tuple[List[int], np.ndarray]:
+        """
+        - Get difference between two dataframes
+        - this method assumes layer1_data is larger than layer2_data
+        """
+        ptr1, ptr2 = 0, 0
+        differing_indices = []
+        diff_data = []
+
+        while ptr1 < len(layer1_data) and ptr2 < len(layer2_data):
+            diff = layer1_data[ptr1] - layer2_data[ptr2]
+            if diff.any():  # if there is any difference between the two values
+                differing_indices.append(ptr1)
+                diff_data.append((layer1_data[ptr1]).tolist())
+                ptr1 += 1
+            else:  # the two values are equal, proceed
+                ptr1 += 1
+                ptr2 += 1
+
+        while ptr1 < len(layer1_data):
+            differing_indices.append(ptr1)
+            diff_data.append((layer1_data[ptr1]).tolist())
+            ptr1 += 1
+
+        res = (differing_indices, np.array(diff_data))
+        logger.info(res)
+        return res
