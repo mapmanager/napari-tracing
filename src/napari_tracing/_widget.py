f1ca58e0ebb1f360c182471b47b39fd09bdc7e79
Vasudha Jha <reachvasudha27@gmail.com>
2023-01-08 18:21:00 -0800

    trace path using brightest-path-lib and napari thread worker

--- src/napari_tracing/_widget.py
+++ src/napari_tracing/_widget.py
@@ -1,24 +1,37 @@
 """
 TODO:
 GUI related:
+- Reset points layer by removing start and end points when Cancel is clicked
 - Read point size (for path width) from a textbox?
+- Read trace path color and size from input
 - Save trace button?
 - reset button?
 - restore edge color and point size after plotting complete to prev color/size
 """
-import warnings
-from collections import defaultdict
-from queue import PriorityQueue
-from typing import Dict, List, Optional, Tuple
+import sys
 
-import numpy as np
-from napari.layers import Layer
-from napari.utils.events import Event
-from napari.viewer import Viewer
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+sys.path.append("/Users/vasudhajha/Documents/mapmanager/brightest-path-lib")
+sys.path.append(
+    "/Users/vasudhajha/Documents/mapmanager/brightest-path-lib/brightest_path_lib"  # noqa
+)
+sys.path.append(
+    "/Users/vasudhajha/Documents/mapmanager/brightest-path-lib/brightest_path_lib/algorithm"  # noqa
+)
+import warnings  # noqa
+from typing import List, Optional  # noqa
 
-from ._logger import logger
-from .utils import Utils
+import napari  # noqa
+import numpy as np  # noqa
+from algorithm import AStarSearch  # noqa
+from brightest_path_lib.algorithm import AStarSearch  # noqa
+from napari.layers import Layer  # noqa
+from napari.qt.threading import thread_worker  # noqa
+from napari.utils.events import Event  # noqa
+from napari.viewer import Viewer  # noqa
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget  # noqa
+
+from ._logger import logger  # noqa
+from .utils import Utils  # noqa
 
 RED = np.array([1, 0, 0, 1])
 GREEN = np.array([0, 1, 0, 1])
@@ -45,13 +58,14 @@ class TracerWidget(QWidget):
 
         self.viewer = viewer
         self.layer = None
-        self.layer_data = np.array([])
-        self.start = None
-        self.end = None
+        self._points_layer_data = np.array([])
+        self._image_layer_data = np.array([])
+        self.start_point = None
+        self.goal_point = None
         self.start_idx = -1
         self.end_idx = -1
-        self.xrange = None
-        self.yrange = None
+        self.worker = None
+
         self.configure_gui()
 
         if layer:
@@ -76,171 +90,18 @@ class TracerWidget(QWidget):
 
     def configure_gui(self) -> None:
         """
-        Configure a QHBoxLayout to hold the undo and redo buttons.
+        Configure a QHBoxLayout to hold the trace and cancel buttons.
         """
         layout = QHBoxLayout()
         trace_button = QPushButton("Trace")
-        trace_button.clicked.connect(self.trace)
+        trace_button.clicked.connect(self.trace_brightest_path)
         layout.addWidget(trace_button)
+
+        cancel_button = QPushButton("Cancel")
+        cancel_button.clicked.connect(self.cancel_tracing)
+        layout.addWidget(cancel_button)
+
         self.setLayout(layout)
-
-    def default_value(self) -> float:
-        return float("inf")
-
-    def calculate_h_score(self, p1: Tuple, p2: Tuple) -> float:
-        """
-        Returns the euclidean distance between two points
-        """
-        x1, y1 = p1
-        x2, y2 = p2
-        return abs(x1 - x2) + abs(y1 - y2)
-        # return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
-    def is_close_to_end(
-        self, point: Tuple, error_threshold: float = 1.0
-    ) -> bool:
-        dist = abs(point[0] - self.end[0]) + abs(point[1] - self.end[1])
-        return dist <= error_threshold
-
-    def set_ranges(self) -> None:
-        """
-        setter function for self.xrange and self.yrange
-        """
-        y_start, x_start = self.start
-        y_end, x_end = self.end
-        self.yrange = (min(y_start, y_end), max(y_start, y_end))
-        self.xrange = (min(x_start, x_end), max(x_start, x_end))
-
-    def get_neighbors(self, point: Tuple) -> List[Tuple]:
-        """
-        get left, right, top and bottom neighbors of a point
-        (When h-func calcs manhattan distance)
-
-        TODO:
-        - later adds checks for barriers
-        - Might need to calculate diagonal neighbors if h func calculates
-        eucledian distance
-        """
-        y, x = point
-        y_min, y_max = self.yrange
-        x_min, x_max = self.xrange
-        neighbors = []
-
-        # top
-        if y > y_min:
-            neighbors.append((y - STEP_SIZE, x))
-
-        # bottom
-        if y < y_max - ERROR:
-            neighbors.append((y + STEP_SIZE, x))
-
-        # left
-        if x > x_min:
-            neighbors.append((y, x - STEP_SIZE))
-
-        # right
-        if x < x_max - ERROR:
-            neighbors.append((y, x + STEP_SIZE))
-
-        logger.info(f"found {len(neighbors)} neighbors")
-        return neighbors
-
-    def trace(self) -> bool:
-        """
-        A* tracing algorithm
-        """
-        logger.info("Starting trace...")
-        if self.start is None or self.end is None:
-            logger.info("Cannot trace without start/end points.")
-            return False
-
-        self.set_ranges()
-        count = 0
-        open_set = PriorityQueue()
-        open_set.put(
-            (0, count, self.start)
-        )  # distance, time of occurrence, point tuple
-        came_from = {}
-        g_score = defaultdict(self.default_value)
-        g_score[self.start] = 0
-        f_score = defaultdict(self.default_value)
-        f_score[self.start] = self.calculate_h_score(self.start, self.end)
-
-        open_set_hash = {self.start}
-
-        while not open_set.empty():
-            element = open_set.get()
-            current = element[2]
-            logger.info(f"inside while. current: {current}")
-            open_set_hash.remove(current)
-
-            # if current == self.end:
-            if self.is_close_to_end(current):
-                self.draw_path(came_from, current)
-                self.change_color(self.end_idx, TURQUOISE)
-                self.change_color(self.start_idx, ORANGE)
-                logger.info("found end point")
-                return True
-
-            neighbors = self.get_neighbors(current)
-            logger.info("found neighbors")
-
-            for neighbor in neighbors:
-                temp_g_score = g_score[current] + STEP_SIZE
-
-                if temp_g_score < g_score[neighbor]:
-                    came_from[neighbor] = current
-                    g_score[neighbor] = temp_g_score
-                    f_score[neighbor] = temp_g_score + self.calculate_h_score(
-                        neighbor, self.end
-                    )
-                    if neighbor not in open_set_hash:
-                        count += 1
-                        # index = self.add_point(
-                        #     np.array(neighbor), GREEN
-                        # )  # make open
-                        open_set.put((f_score[neighbor], count, neighbor))
-                        open_set_hash.add(neighbor)
-
-            # draw()
-
-            # if current != self.start:
-            #     # self.add_point(np.array(current), RED) # make closed
-            #     # self.add_point(np.array(current)) # make closed
-            #     self.change_color(index, RED)
-
-        logger.info("Done tracing")
-        return False
-
-    def add_point(self, point: np.ndarray, color: Optional[np.ndarray] = None):
-        """
-        add a new point in the napari layer
-        """
-        self.layer_data = np.append(self.layer_data, np.array([point]), axis=0)
-        self.layer.data = self.layer_data
-        index = len(self.layer_data) - 1
-
-        if color is not None:
-            self.change_color(index, color)
-
-        self.layer.refresh()
-
-    def change_color(self, idx: int, color: np.array) -> None:
-        """
-        change the color of a point in the layer
-        """
-        layer_colors = self.layer._face.colors
-        layer_colors[idx] = color
-        self.layer._face.current_color = WHITE
-        self.layer.refresh()
-
-    def draw_path(self, came_from: Dict[Tuple, Tuple], current: Tuple) -> None:
-        """
-        Draws final path by changing color of points in the path to purple.
-        """
-        while current in came_from:
-            self.add_point(np.array(current), PURPLE)
-            current = came_from[current]
 
     def find_active_layers(self) -> Optional[Layer]:
         """
@@ -260,14 +121,26 @@ class TracerWidget(QWidget):
                         layer: Layer (Layer to connect to.)
         """
         if self.layer:
-            self.layer.events.data.disconnect(self.slot_points_data_change)
-            # self.layer.events.highlight.disconnect(self.slot_user_selected_point)
-            self.layer_data = np.array([])
+            if isinstance(self.layer, napari.layers.points.points.Points):
+                self.layer.events.data.disconnect(self.slot_points_data_change)
+                # self.layer.events.highlight.disconnect(self.slot_user_selected_point)
+                self._points_layer_data = np.array([])
 
         self.layer = layer
-        if "data" in vars(layer).keys() and layer.data.any():
-            self.layer_data = layer.data.copy()
-        self.layer.events.data.connect(self.slot_points_data_change)
+        logger.info(f"Found new layer of type {type(self.layer)}")
+
+        if isinstance(self.layer, napari.layers.points.points.Points):
+            # if "data" in vars(layer).keys() and layer.data.any():
+            if "_data" in vars(layer).keys() and layer.data.any():
+                logger.info("Getting points layer data")
+                self._points_layer_data = layer.data.copy()
+            self.layer.events.data.connect(self.slot_points_data_change)
+
+        elif isinstance(self.layer, napari.layers.image.image.Image):
+            if "_data" in vars(layer).keys() and layer.data.any():
+                logger.info("Getting image layer data")
+                self._image_layer_data = layer.data.copy()
+
         # self.layer.events.highlight.connect(self.slot_user_selected_point)
 
     def slot_points_data_change(self, event: Event) -> None:
@@ -275,46 +148,52 @@ class TracerWidget(QWidget):
         Modify the start/end points for a* search
         based on point adding/deletion
         """
-        if Utils.are_sets_equal(event.source.selected_data, self.layer_data):
+        logger.info("Inside slot_points_data_change")
+
+        if Utils.are_sets_equal(
+            event.source.selected_data, self._points_layer_data
+        ):
             # no change
             return
 
-        if len(event.source.data) > len(self.layer_data):
+        if len(event.source.data) > len(self._points_layer_data):
             logger.info("new point added")
-            idx, point = Utils.get_diff(event.source.data, self.layer_data)
+            idx, point = Utils.get_diff(
+                event.source.data, self._points_layer_data
+            )
             # point = tuple(map(int, tuple(point[0])))
             point = tuple(point[0])
 
-            if self.start is None and point != self.end:
+            if self.start_point is None and point != self.goal_point:
                 logger.info(f"Making {point} as our start")
                 self.start_idx = len(event.source.data) - 1
-                self.start = point
+                self.start_point = point
                 self.change_color(idx, ORANGE)
 
-            elif self.end is None and point != self.start:
+            elif self.goal_point is None and point != self.start_point:
                 logger.info(f"Making {point} as our end")
                 self.end_idx = len(event.source.data) - 1
-                self.end = point
+                self.goal_point = point
                 self.change_color(idx, TURQUOISE)
 
-            self.layer_data = event.source.data.copy()
+            self._points_layer_data = event.source.data.copy()
 
-        elif len(event.source.data) < len(self.layer_data):
+        elif len(event.source.data) < len(self._points_layer_data):
             logger.info("point is deleted")
             idx, point = Utils.get_diff(
-                self.layer_data, event.source.data.copy()
+                self._points_layer_data, event.source.data.copy()
             )
             point = tuple(point[0])
 
-            if point == self.start:
+            if point == self.start_point:
                 logger.info("resetting start since it was deleted")
-                self.start = None
+                self.start_point = None
 
-            elif point == self.end:
+            elif point == self.goal_point:
                 logger.info("resetting end since it was deleted")
-                self.end = None
+                self.goal_point = None
 
-            self.layer_data = event.source.data.copy()
+            self._points_layer_data = event.source.data.copy()
 
     def slot_insert_layer(self, event: Event) -> None:
         """
@@ -352,3 +231,66 @@ class TracerWidget(QWidget):
         if currently_selected_layer and currently_selected_layer != self.layer:
             logger.info(f"New layer selected: {currently_selected_layer}")
             self.connect_layer(currently_selected_layer)
+
+    @thread_worker
+    def _trace(self):
+        if len(self._image_layer_data) == 0:
+            logger.info("No image data to trace")
+            return
+
+        tracing_algorithm = AStarSearch(
+            self._image_layer_data, self.start_point, self.goal_point
+        )
+
+        result = tracing_algorithm.search()
+        logger.info(f"Completed tracing. Found path of length {len(result)}")
+        return result
+
+    def trace_brightest_path(self):
+        """call the brightest_path_lib to find brightest path
+        between start_point and goal_point
+        """
+        self.worker = self._trace()
+        self.worker.returned.connect(self.plot_brightest_path)
+        self.worker.start()
+
+    def plot_brightest_path(self, points: List[np.ndarray]):
+        logger.info("Plotting brightest path...")
+        self.viewer.add_points(
+            points,
+            size=1,
+            edge_width=1,
+            face_color="green",
+            edge_color="green",
+        )
+
+    def cancel_tracing(self):
+        """Cancel brightest path tracing"""
+        if self.worker:
+            logger.info("Cancelling tracing...")
+            self.worker.quit()
+            self.worker = None
+
+    def add_point(self, point: np.ndarray, color: Optional[np.ndarray] = None):
+        """
+        add a new point in the napari layer
+        """
+        self._points_layer_data = np.append(
+            self._points_layer_data, np.array([point]), axis=0
+        )
+        self.layer.data = self._points_layer_data
+        index = len(self._points_layer_data) - 1
+
+        if color is not None:
+            self.change_color(index, color)
+
+        self.layer.refresh()
+
+    def change_color(self, idx: int, color: np.array) -> None:
+        """
+        change the color of a point in the layer
+        """
+        layer_colors = self.layer._face.colors
+        layer_colors[idx] = color
+        self.layer._face.current_color = WHITE
+        self.layer.refresh()
